4cf8d6b588dae4a11490a0eae0efc4432974bbaf
Zain Ejaz <90384001+zainbhatti123@users.noreply.github.com>
2022-01-28 12:37:14 +0500

    Delete server/server directory

--- server/server/key.js
+++
@@ -1,4 +0,0 @@
-module.exports ={
-    MONGOURI:'mongodb+srv://ahsan:JtHLMNqogjsN9tZS@cluster0.b7yse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    JWT_SECRET:'mydfjkdbfkjbkjdbfkbfsd123'
-}
